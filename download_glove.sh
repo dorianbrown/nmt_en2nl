5385210eaa157310a2e1598dec34da3f185d1752
Dorian Brown <dorian.brown@ing.com>
2019-03-07 10:32:18 +0100

    Added parallel corpus downloading

--- download_glove.sh
+++ download_glove.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+echo "Downloading glove word embeddings"
 mkdir -p data/glove
 wget http://nlp.stanford.edu/data/glove.6B.zip
 unzip glove.6B.zip -d data/glove
 rm glove.6B.zip
+
+echo "Downloading Europarl Dutch-English parallel corpus"
+mkdir -p data/corpus
+tar -xvf nl-en.tar -C data/corpus
+rm nl-en.tar
