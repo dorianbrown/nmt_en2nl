d400377dcdd92b5c6617a430c4b3bf49cbd59ebd
Dorian Brown <dorian.brown@ing.com>
2019-03-07 10:33:54 +0100

    Updated loader path

--- preprocessing.py
+++ preprocessing.py
@@ -8,22 +8,22 @@ def load_sentences(file_dir, max_vocab = 20_000, max_seq_len=100, max_n=1e9):
     with open(file_dir, 'r') as f:
         lines = f.readlines()
     lines = [l.strip() for l in lines]
-        
+
     lines = lines[:min(len(lines), max_n)]
-    
+
     tokenizer = Tokenizer(num_words=max_vocab)
     tokenizer.fit_on_texts(lines)
     sequences = tokenizer.texts_to_sequences(lines)
-    
+
     vocab = tokenizer.word_index
     data = pad_sequences(sequences, maxlen=max_seq_len)
-    
+
     return vocab, lines, data
 
 def load_nlen_pairs():
-    en_vocab, en_text, en_data = load_sentences('data/europarl-v7.nl-en.en')
-    nl_vocab, nl_text, nl_data = load_sentences('data/europarl-v7.nl-en.nl')
-    
+    en_vocab, en_text, en_data = load_sentences('data/corpus/europarl-v7.nl-en.en')
+    nl_vocab, nl_text, nl_data = load_sentences('data/corpus/europarl-v7.nl-en.nl')
+
     en = {
         'vocab': en_vocab,
         'text': en_text,
@@ -34,5 +34,5 @@ def load_nlen_pairs():
         'text': nl_text,
         'data': nl_data
     }
-    
+
     return en, nl
